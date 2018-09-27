7ab9c961befa184d837484a33317781619a612f0
prashant.p <prashant.p@mtmdvprashantp.internal.directi.com>
2018-09-27 15:30:41 +0530

    Last updated and max-min values updated

--- websockets.js
+++ websockets.js
@@ -32,42 +32,105 @@ function renderStocks(wsMsg) {
     var newArray = JSON.parse(wsMsg);
 
     function getNewRow(id) {
-        var tr = document.createElement("tr");
-        tr.setAttribute("id", id);
-        return tr;
+        var div = document.createElement("div");
+        div.setAttribute("id", id);
+        div.setAttribute("class", "row");
+        return div;
     }
 
     var table = document.getElementById("results");
 
     newArray.forEach(function (stockData) {
-        var name = stockData[0],
-            price = stockData[1],
-            time = Date.now();
+        var name = stockData[0];
+
         if(!stockObj[name]) {
             table.appendChild(getNewRow(name));
         }
         var stockElement = document.getElementById(name);
-        stockObj[name] = {
-            name: name,
-            price: price,
-            time: time
-        };
+        updateStockObj(stockData, stockObj[name] || {});
         updateRow(stockElement, stockObj[name])
     });
+}
+
+function updateStockObj(stockData, stock) {
+    var name = stockData[0],
+        price = stockData[1],
+        max = stock.max || price,
+        min = stock.min || price,
+        presentTime = Date.now(),
+        time = stock.time || presentTime,
+        priceRise = false,
+        priceFall = false,
+        displayTime = time;
+
+    if(stock) {
+        if(max<price) {
+            max = price;
+            priceRise = true;
+        } else if(min>price) {
+            min = price;
+            priceFall = true;
+        }
+
+        if(time <= presentTime) {
+            displayTime = Math.abs(presentTime - time)/1000;
+        }
+    }
+
+    stockObj[name] = {
+        name: name,
+        price: price,
+        max: max,
+        min: min,
+        priceRise: priceRise,
+        priceFall: priceFall,
+        time: presentTime,
+        displayTime: displayTime
+    };
 }
 
 function updateRow(element, data) {
     element.innerHTML = "";
 
-    var td1 = document.createElement("td");
-    var td2 = document.createElement("td");
-    var td3 = document.createElement("td");
+    var name = document.createElement("div");
+    var price = document.createElement("div");
+    var time = document.createElement("div");
+    var max = document.createElement("div");
+    var min = document.createElement("div");
 
-    td1.innerText = data.name;
-    td2.innerText = data.price;
-    td3.innerText = data.time;
+    time.setAttribute("id", name + "-time");
 
-    element.appendChild(td1);
-    element.appendChild(td2);
-    element.appendChild(td3);
+    name.innerText = data.name;
+    price.innerText = data.price;
+    time.innerText = Math.round(data.displayTime)==0? "Jus Now" : "Updated " + Math.round(data.displayTime) + " secs ago";
+    max.innerText = data.max;
+    min.innerText = data.min;
+
+    element.appendChild(name);
+    element.appendChild(price);
+    element.appendChild(time);
+    element.appendChild(max);
+    element.appendChild(min);
+
+    if(data.priceRise) {
+        element.classList.add("priceRise");
+    } else if(data.priceFall) {
+        element.classList.add("priceFall");
+    } else {
+        element.classList.remove("priceRise");
+        element.classList.remove("priceFall");
+    }
 }
+
+setInterval(function () {
+    var presentTime = Date.now();
+    var names = Object.keys(stockObj);
+
+    names.forEach(function(stock) {
+        if(presentTime - stockObj[stock].time > 60000) {
+            stockObj[stock].displayTime = stockObj[stock].displayTime + 60;
+
+            document.getElementById(stock+"-time").innerText = stockObj[stock].displayTime;
+        }
+    });
+}, 60000);
